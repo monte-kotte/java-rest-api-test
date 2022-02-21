081bf69a895bf75915464ce9da3d0980bddbb200
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2022-02-21 17:05:55 -0500

    Add testcontainer for mongoDB

--- service-user/src/test/java/monte/test/AbstractTest.java
+++ service-user/src/test/java/monte/test/AbstractTest.java
@@ -4,12 +4,28 @@ import monte.service.user.UserApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("component-test")
 @SpringBootTest(classes = UserApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 public class AbstractTest {
+
+    @Container
+    public static MongoDBContainer mongoDbContainer =
+            new MongoDBContainer(DockerImageName.parse("mongo"));
+
+    @DynamicPropertySource
+    public static void dynamicProperty(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDbContainer::getReplicaSetUrl);
+    }
 
     @Test
     void test() {
