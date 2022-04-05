f7c1b2e16c22005506ab438a7994ad2d2ce72c69
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2022-04-05 16:36:35 +0200

    Add test model for integration tests

---
+++ integration-test/src/test/java/monte/test/model/api/TestApiDL.java
@@ -0,0 +1,15 @@
+package monte.test.model.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestApiDL {
+    private String number;
+    private Boolean active;
+}
---
+++ integration-test/src/test/java/monte/test/model/api/error/TestValidationErrorResponse.java
@@ -0,0 +1,17 @@
+package monte.test.model.api.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestValidationErrorResponse {
+    List<TestViolation> violations = new ArrayList<>();
+}
