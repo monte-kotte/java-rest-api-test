148757071ebbf40e723923aa991ff2bf67f8dfe0
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2022-04-05 17:10:58 +0200

    Use dynamic test profiles

--- integration-test/src/test/java/monte/test/utils/ConfigFileReader.java
+++ integration-test/src/test/java/monte/test/utils/ConfigFileReader.java
@@ -1,35 +1,36 @@
 package monte.test.utils;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigFileReader {
 
-    private static Properties properties;
-    private static final String PROPERTY_FILE_PATH= "src/test/resources/test-local.properties";
+    private static Properties properties = new Properties();
+    public static final String DEFAULT_VALUE = "";
+    public static final String DEFAULT_PROFILE = "local";
+
+    private static final String PROPERTY_FILE_PATH_TEMPLATE = "src/test/resources/test-${env}.properties";
     private static final String USER_SERVICE_URL_PROPERTY = "service.user.url";
 
     static {
         try {
-            var reader = new BufferedReader(new FileReader(PROPERTY_FILE_PATH));
-            properties = new Properties();
-            try {
-                properties.load(reader);
-                reader.close();
-            } catch (IOException e) {
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Property file not found at " + PROPERTY_FILE_PATH);
+            var fileName = getPropertyFilePath();
+            var reader = new BufferedReader(new FileReader(fileName));
+            properties.load(reader);
+            reader.close();
+        } catch (Exception e) {
+            throw new RuntimeException("Property file could not be used");
         }
     }
 
     public static String getUserServiceUrl() {
-        var url = properties.getProperty(USER_SERVICE_URL_PROPERTY);
-        if(url != null) return url;
-        else throw new RuntimeException("Url not specified in the property file " + PROPERTY_FILE_PATH);
+        return properties.getProperty(USER_SERVICE_URL_PROPERTY, DEFAULT_VALUE);
+    }
+
+    private static String getPropertyFilePath() {
+        var env = System.getProperty("env", DEFAULT_PROFILE);
+        return PROPERTY_FILE_PATH_TEMPLATE.replace("${env}", env);
     }
 
 }
