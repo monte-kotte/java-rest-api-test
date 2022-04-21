d5971a58b3a7f09ca5325408bfefd5833289bf6e
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2022-04-21 13:33:19 +0200

    Add connection to mongoDB

--- integration-test/src/test/java/monte/test/steps/AbstractSteps.java
+++ integration-test/src/test/java/monte/test/steps/AbstractSteps.java
@@ -1,13 +1,17 @@
 package monte.test.steps;
 
+
 import monte.test.context.TestContext;
+import monte.test.utils.ComponentFactory;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import static monte.test.context.TestContext.CONTEXT;
 
 public abstract class AbstractSteps {
 
-    TestRestTemplate restTemplate = new TestRestTemplate();
+    TestRestTemplate restTemplate = ComponentFactory.createTestRestTemplate();
+    MongoTemplate mongoTemplate = ComponentFactory.createMongoTemplate();
 
     public TestContext testContext() {
         return CONTEXT;
