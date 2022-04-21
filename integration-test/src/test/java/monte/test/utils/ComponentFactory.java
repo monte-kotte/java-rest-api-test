0efa22293528a662eb7f37c2ed89147bac67d359
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2022-04-21 13:33:19 +0200

    Add connection to mongoDB

---
+++ integration-test/src/test/java/monte/test/utils/ComponentFactory.java
@@ -0,0 +1,46 @@
+package monte.test.utils;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.Arrays;
+
+public class ComponentFactory {
+
+    public static TestRestTemplate createTestRestTemplate() {
+        return new TestRestTemplate();
+    }
+
+    public static MongoTemplate createMongoTemplate() {
+        var serverAddress = createServerAddress();
+        var credential = createMongoCredential();
+        return new MongoTemplate(createMongoClient(serverAddress, credential), ConfigFileReader.getMongoDbName());
+    }
+
+    private static ServerAddress createServerAddress() {
+        return new ServerAddress(ConfigFileReader.getMongoHost(),
+                Integer.parseInt(ConfigFileReader.getMongoPort()));
+    }
+
+    private static MongoCredential createMongoCredential() {
+        return MongoCredential.createCredential(
+                ConfigFileReader.getMongoUsername(),
+                ConfigFileReader.getMongoAuthenticationDb(),
+                ConfigFileReader.getMongoPassword().toCharArray());
+    }
+
+    private static MongoClient createMongoClient(ServerAddress serverAddress, MongoCredential credential) {
+        return MongoClients.create(
+                MongoClientSettings.builder()
+                        .applyToClusterSettings(builder ->
+                                builder.hosts(Arrays.asList(serverAddress)))
+                        .credential(credential)
+                        .build());
+    }
+
+}
--- integration-test/src/test/java/monte/test/utils/ConfigFileReader.java
+++ integration-test/src/test/java/monte/test/utils/ConfigFileReader.java
@@ -12,6 +12,12 @@ public class ConfigFileReader {
 
     private static final String PROPERTY_FILE_PATH_TEMPLATE = "src/test/resources/test-${env}.properties";
     private static final String USER_SERVICE_URL_PROPERTY = "service.user.url";
+    private static final String MONGODB_HOST_PROPERTY = "mongodb.host";
+    private static final String MONGODB_PORT_PROPERTY = "mongodb.port";
+    private static final String MONGODB_AUTHENTICATION_DB_PROPERTY = "mongodb.authentication-database";
+    private static final String MONGODB_USERNAME_PROPERTY = "mongodb.username";
+    private static final String MONGODB_PASSWORD_PROPERTY = "mongodb.password";
+    private static final String MONGODB_DATABASE_PROPERTY = "mongodb.database";
 
     static {
         try {
@@ -26,6 +32,30 @@ public class ConfigFileReader {
 
     public static String getUserServiceUrl() {
         return properties.getProperty(USER_SERVICE_URL_PROPERTY, DEFAULT_VALUE);
+    }
+
+    public static String getMongoHost() {
+        return properties.getProperty(MONGODB_HOST_PROPERTY, DEFAULT_VALUE);
+    }
+
+    public static String getMongoPort() {
+        return properties.getProperty(MONGODB_PORT_PROPERTY, DEFAULT_VALUE);
+    }
+
+    public static String getMongoAuthenticationDb() {
+        return properties.getProperty(MONGODB_AUTHENTICATION_DB_PROPERTY, DEFAULT_VALUE);
+    }
+
+    public static String getMongoUsername() {
+        return properties.getProperty(MONGODB_USERNAME_PROPERTY, DEFAULT_VALUE);
+    }
+
+    public static String getMongoPassword() {
+        return properties.getProperty(MONGODB_PASSWORD_PROPERTY, DEFAULT_VALUE);
+    }
+
+    public static String getMongoDbName() {
+        return properties.getProperty(MONGODB_DATABASE_PROPERTY, DEFAULT_VALUE);
     }
 
     private static String getPropertyFilePath() {
