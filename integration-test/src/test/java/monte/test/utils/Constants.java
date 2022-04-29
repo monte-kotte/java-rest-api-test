54857ea5968d75e36859b374bd0685960a66f6fe
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2022-04-29 09:52:37 +0200

    Refactoring

--- integration-test/src/test/java/monte/test/utils/Constants.java
+++ integration-test/src/test/java/monte/test/utils/Constants.java
@@ -1,13 +1,7 @@
 package monte.test.utils;
 
 public final class Constants {
-
     public final static class TestData {
         public static final String ID_TO_REPLACE = "REPLACE_ME_WITH_REAL_ID";
-
-        public static final String AUDIT_READ_COMMENT = "read user";
-        public static final String AUDIT_CREATE_COMMENT = "create new user";
-        public static final String AUDIT_UPDATE_COMMENT = "update user";
     }
-
 }
