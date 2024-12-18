22ff706d9353847f4d1e0a76726f14d34b477805
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2024-12-18 19:05:18 +0100

    Update mongodb connection settings

--- integration-test/src/test/java/monte/test/utils/ConfigFileReader.java
+++ integration-test/src/test/java/monte/test/utils/ConfigFileReader.java
@@ -12,12 +12,8 @@ public class ConfigFileReader {
 
     private static final String PROPERTY_FILE_PATH_TEMPLATE = "src/test/resources/test-${env}.properties";
     private static final String USER_SERVICE_URL_PROPERTY = "service.user.url";
-    private static final String MONGODB_HOST_PROPERTY = "mongodb.host";
-    private static final String MONGODB_PORT_PROPERTY = "mongodb.port";
-    private static final String MONGODB_AUTHENTICATION_DB_PROPERTY = "mongodb.authentication-database";
-    private static final String MONGODB_USERNAME_PROPERTY = "mongodb.username";
-    private static final String MONGODB_PASSWORD_PROPERTY = "mongodb.password";
-    private static final String MONGODB_DATABASE_PROPERTY = "mongodb.database";
+    private static final String MONGODB_DATABASE_PROPERTY = "mongodb.database.name";
+    private static final String MONGODB_DATABASE_CONNECTION_STRING_PROPERTY = "mongodb.database.connection.string";
 
     static {
         try {
@@ -34,33 +30,16 @@ public class ConfigFileReader {
         return properties.getProperty(USER_SERVICE_URL_PROPERTY, DEFAULT_VALUE);
     }
 
-    public static String getMongoHost() {
-        return properties.getProperty(MONGODB_HOST_PROPERTY, DEFAULT_VALUE);
-    }
-
-    public static String getMongoPort() {
-        return properties.getProperty(MONGODB_PORT_PROPERTY, DEFAULT_VALUE);
-    }
-
-    public static String getMongoAuthenticationDb() {
-        return properties.getProperty(MONGODB_AUTHENTICATION_DB_PROPERTY, DEFAULT_VALUE);
-    }
-
-    public static String getMongoUsername() {
-        return properties.getProperty(MONGODB_USERNAME_PROPERTY, DEFAULT_VALUE);
-    }
-
-    public static String getMongoPassword() {
-        return properties.getProperty(MONGODB_PASSWORD_PROPERTY, DEFAULT_VALUE);
-    }
-
     public static String getMongoDbName() {
         return properties.getProperty(MONGODB_DATABASE_PROPERTY, DEFAULT_VALUE);
+    }
+
+    public static String getMongoDbConnectionString(){
+        return properties.getProperty(MONGODB_DATABASE_CONNECTION_STRING_PROPERTY);
     }
 
     private static String getPropertyFilePath() {
         var env = System.getProperty("env", DEFAULT_PROFILE);
         return PROPERTY_FILE_PATH_TEMPLATE.replace("${env}", env);
     }
-
 }
